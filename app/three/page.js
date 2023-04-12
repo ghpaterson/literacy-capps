91c1c5d169e19886ec96e42360307c8081188b5d
Graeme Paterson <ghpaterson@gmail.com>
2023-04-12 14:31:42 +0100

    created boxes and generated function to randomly assign letter to first box and middle box

--- app/three/page.js
+++ app/three/page.js
@@ -1,7 +1,69 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Three() {
-    return (
+  const [vowel, setVowel] = useState(null);
+  const vowels = ["a", "e", "i", "o", "u"];
+
+  const [firstLetter, setFirstLetter] = useState(null);
+  const firstBox = [
+    "b",
+    "c",
+    "d",
+    "f",
+    "g",
+    "h",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "p",
+    "r",
+    "s",
+    "t",
+    "v",
+    "w",
+    "y",
+    "z",
+  ];
+
+  const handleVowelClick = () => {
+    const newVowel = vowels[Math.floor(Math.random() * vowels.length)];
+    setVowel(newVowel);
+  };
+
+  const handleFirstBoxClick = () => {
+    const newFirstLetter =
+      firstBox[Math.floor(Math.random() * firstBox.length)];
+    setFirstLetter(newFirstLetter);
+  };
+
+  return (
+    <>
+      <main>
         <div className="flex justify-center">
-            <h1 className="text-2xl lg:text-5xl">Let's Spell a Three Letter Word</h1>
+          <h1 className="text-2xl lg:text-5xl">
+            Let's Spell a Three Letter Word
+          </h1>
         </div>
-    )
+        <div className="flex justify-center gap-14 my-10">
+          <div
+            className=" flex justify-center items-center h-60 w-60 bg-gray-100 text-gray-900 text-6xl"
+            onClick={handleFirstBoxClick}
+          >
+            {firstLetter}
+          </div>
+          <div
+            className=" flex justify-center items-center h-60 w-60 bg-gray-100 text-red-500 text-6xl"
+            onClick={handleVowelClick}
+          >
+            {vowel}
+          </div>
+          <div className="h-60 w-60 bg-gray-100"></div>
+        </div>
+      </main>
+    </>
+  );
 }
