a7e021ef4c2465721ecf16677abed7ed48b89b82
Graeme Paterson <ghpaterson@gmail.com>
2023-04-22 13:51:29 +0100

    changed the exercise buttons

--- app/Menu/page.js
+++ app/Menu/page.js
@@ -6,26 +6,56 @@ export default function Menu() {
     <>
       <main className=" bg-tiffany h-screen">
         <NavBar />
-        <div className="flex justify-center">
+        <div className="flex justify-center py-10">
           <div className="grid grid-cols-1 md:grid-cols-3 gap-10 mx-auto text-xl">
-            <div className="bg-transparent rounded-xl shadow-lg py-6 px-6">
-              <Link href="/CVC">Exercise 1</Link>
-            </div>
-            <div className="bg-transparent rounded-xl shadow-lg py-6 px-6">
-              <Link href="/CCVCa">Exercise 2a</Link>
-            </div>
-            <div className="bg-transparent rounded-xl shadow-lg py-6 px-6">
-              <Link href="/CCVCb">Exercise 2b</Link>
-            </div>
-            <div className="bg-transparent rounded-xl shadow-lg py-6 px-6">
-              <Link href="/CVCC">Exercise 3</Link>
-            </div>
-            <div className="bg-transparent rounded-xl shadow-lg py-6 px-6">
-              <Link href="/CCVCC">Exercise 4</Link>
-            </div>
-            <div className="bg-transparent rounded-xl shadow-lg py-6 px-6">
-              <Link href="/CCCVCCC">Exercise 5</Link>
-            </div>
+            <Link href="/CVC">
+              <div class="bg-transparent rounded-xl shadow-lg py-6 px-6 cursor-pointer">
+                <div>Exercise 1</div>
+                <div class="bg-white rounded-md py-2 px-4 my-2">
+                  <p>Read a Three Letter Word</p>
+                </div>
+              </div>
+            </Link>
+            <Link href="/CCVAa">
+              <div class="bg-transparent rounded-xl shadow-lg py-6 px-6 cursor-pointer">
+                <div>Exercise 2a</div>
+                <div class="bg-white rounded-md py-2 px-4 my-2">
+                  <p>Read a Four Letter Word</p>
+                </div>
+              </div>
+            </Link>
+            <Link href="/CCVCb">
+              <div class="bg-transparent rounded-xl shadow-lg py-6 px-6 cursor-pointer">
+                <div>Exercise 2b</div>
+                <div class="bg-white rounded-md py-2 px-4 my-2">
+                  <p>Read a Four Letter Word</p>
+                </div>
+              </div>
+            </Link>
+            <Link href="/CVCC">
+              <div class="bg-transparent rounded-xl shadow-lg py-6 px-6 cursor-pointer">
+                <div>Exercise 3</div>
+                <div class="bg-white rounded-md py-2 px-4 my-2">
+                  <p>Read a Four Letter Word</p>
+                </div>
+              </div>
+            </Link>
+            <Link href="/CCVCC">
+              <div class="bg-transparent rounded-xl shadow-lg py-6 px-6 cursor-pointer">
+                <div>Exercise 4</div>
+                <div class="bg-white rounded-md py-2 px-4 my-2">
+                  <p>Read a Five Letter Word</p>
+                </div>
+              </div>
+            </Link>
+            <Link href="/CCCVCCC">
+              <div class="bg-transparent rounded-xl shadow-lg py-6 px-6 cursor-pointer">
+                <div>Exercise 5</div>
+                <div class="bg-white rounded-md py-2 px-4 my-2">
+                  <p>Read a Seven Letter Word</p>
+                </div>
+              </div>
+            </Link>
           </div>
         </div>
       </main>
