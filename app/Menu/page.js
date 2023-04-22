f86b21d4cd9e5b57a423013b6bbd487012a53b8e
Graeme Paterson <ghpaterson@gmail.com>
2023-04-22 14:10:11 +0100

    created an ExerciseCard component

--- app/Menu/page.js
+++ app/Menu/page.js
@@ -1,61 +1,54 @@
 import NavBar from "@/components/navbar";
-import Link from "next/link";
+import ExerciseCard from "@/components/exerciseCard";
 
 export default function Menu() {
+  const exercises = [
+    {
+      href: "/CVC",
+      title: "Exercise 1",
+      description: "Read a Three Letter Word",
+    },
+    {
+      href: "/CCVCa",
+      title: "Exercise 2a",
+      description: "Read a Four Letter Word",
+    },
+    {
+      href: "/CCVCb",
+      title: "Exercise 2b",
+      description: "Read a Four Letter Word",
+    },
+    {
+      href: "/CVCC",
+      title: "Exercise 3",
+      description: "Read a Four Letter Word",
+    },
+    {
+      href: "/CCVCC",
+      title: "Exercise 4",
+      description: "Read a Five Letter Word",
+    },
+    {
+      href: "/CCCVCCC",
+      title: "Exercise 5",
+      description: "Read a Seven Letter Word",
+    },
+  ];
+
   return (
     <>
-      <main className=" bg-tiffany h-screen">
+      <main className="bg-tiffany h-screen">
         <NavBar />
         <div className="flex justify-center py-10">
           <div className="grid grid-cols-1 md:grid-cols-3 gap-10 mx-auto text-xl">
-            <Link href="/CVC">
-              <div class="bg-bittersweet rounded-xl shadow-lg py-6 px-6 cursor-pointer">
-                <div>Exercise 1</div>
-                <div class="bg-white rounded-md py-2 px-4 my-2">
-                  <p>Read a Three Letter Word</p>
-                </div>
-              </div>
-            </Link>
-            <Link href="/CCVCa">
-              <div class="bg-bittersweet rounded-xl shadow-lg py-6 px-6 cursor-pointer">
-                <div>Exercise 2a</div>
-                <div class="bg-white rounded-md py-2 px-4 my-2">
-                  <p>Read a Four Letter Word</p>
-                </div>
-              </div>
-            </Link>
-            <Link href="/CCVCb">
-              <div class="bg-bittersweet rounded-xl shadow-lg py-6 px-6 cursor-pointer">
-                <div>Exercise 2b</div>
-                <div class="bg-white rounded-md py-2 px-4 my-2">
-                  <p>Read a Four Letter Word</p>
-                </div>
-              </div>
-            </Link>
-            <Link href="/CVCC">
-              <div class="bg-bittersweet rounded-xl shadow-lg py-6 px-6 cursor-pointer">
-                <div>Exercise 3</div>
-                <div class="bg-white rounded-md py-2 px-4 my-2">
-                  <p>Read a Four Letter Word</p>
-                </div>
-              </div>
-            </Link>
-            <Link href="/CCVCC">
-              <div class="bg-bittersweet rounded-xl shadow-lg py-6 px-6 cursor-pointer">
-                <div>Exercise 4</div>
-                <div class="bg-white rounded-md py-2 px-4 my-2">
-                  <p>Read a Five Letter Word</p>
-                </div>
-              </div>
-            </Link>
-            <Link href="/CCCVCCC">
-              <div class="bg-bittersweet rounded-xl shadow-lg py-6 px-6 cursor-pointer">
-                <div>Exercise 5</div>
-                <div class="bg-white rounded-md py-2 px-4 my-2">
-                  <p>Read a Seven Letter Word</p>
-                </div>
-              </div>
-            </Link>
+            {exercises.map((exercise) => (
+              <ExerciseCard
+                key={exercise.href}
+                href={exercise.href}
+                title={exercise.title}
+                description={exercise.description}
+              />
+            ))}
           </div>
         </div>
       </main>
